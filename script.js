937ea8ba8edac008d8e03ff61746f48bbd2528ac
W.Carlos <wendcarlos18@hotmail.com>
2021-04-20 10:28:58 -0300

    Update script.js
    adicionei backgroundColor red, para mudar a cor de fundo caso o motorista seja multado

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ function calcular () {
     var txtv = document.getElementById('txtvel')
     var res = document.getElementById('res')
     var vel = Number(txtv.value)
-    res.innerHTML = `Sua velocidade é de <strong>${vel} Km/h</strong>.`
+    res.innerHTML = `<p>Sua velocidade é de <strong>${vel} Km/h</strong>.</p>`
+
+    if (vel > 60) {
+        res.innerHTML += `Você foi <strong>MULTADO</strong> por excesso de velocidade!`
+        document.body.style.backgroundColor = 'red'
+    }
+
+    res.innerHTML += 'Dirija sempre com cuidado e com o cinto de seguraça.'
 }
