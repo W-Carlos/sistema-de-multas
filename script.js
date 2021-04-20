3c29d8868d4c22363827f8fc85fced48ace96cb2
W.Carlos <wendcarlos18@hotmail.com>
2021-04-20 11:12:28 -0300

    style.css
    Fiz toda estilização da pagina

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function calcular () {
     var vel = Number(txtv.value)
     res.innerHTML = `<p>Sua velocidade é de <strong>${vel} Km/h</strong>.</p>`
 
-    if (vel > 60) {
+    if (vel >= 70) {
         res.innerHTML += `Você foi <strong>MULTADO</strong> por excesso de velocidade!`
         document.body.style.backgroundColor = 'red'
+    } else {
+        res.innerHTML += `Você está dentro do limite de velocidade.`
+        document.body.style.backgroundColor = 'rgb(22, 189, 22)'
     }
 
-    res.innerHTML += 'Dirija sempre com cuidado e com o cinto de seguraça.'
+    res.innerHTML += ' Dirija sempre com cuidado e com o cinto de seguraça.'
 }
