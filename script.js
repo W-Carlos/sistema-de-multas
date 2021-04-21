b5d69d6be1ef2b1db71d953230243053e039166b
W.Carlos <wendcarlos18@hotmail.com>
2021-04-21 11:32:53 -0300

    Update script.js
    adicionei o amarelo para quando a km é perto do limite da via

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ function calcular () {
     var vel = Number(txtv.value)
     res.innerHTML = `<p>Sua velocidade é de <strong>${vel} Km/h</strong>.</p>`
 
-    if (vel >= 70) {
+    if (vel >= 80) {
         res.innerHTML += `Você foi <strong>MULTADO</strong> por excesso de velocidade!`
         document.body.style.backgroundColor = 'red'
+    } else if (vel >= 70 && vel < 80 ) {
+        res.innerHTML += `Você chegou perdo do limite de velocidade. Por favor preste mais atenção.`
+        document.body.style.backgroundColor = 'yellow'
     } else {
         res.innerHTML += `Você está dentro do limite de velocidade.`
         document.body.style.backgroundColor = 'rgb(22, 189, 22)'
